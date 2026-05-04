32a427555049348dd80b4e3398088110970da715
Chengengxie <chengengxie0717@gmail.com>
2026-05-04 19:16:51 +0800

    1

--- kaira-1.0.0/edit_club.php
+++ kaira-1.0.0/edit_club.php
@@ -1,15 +1,7 @@
 <?php
 session_start();
 
-<<<<<<< HEAD
-$host    = "localhost";
-$dbname  = "sa2026";
-$db_user = "root";
-$db_pass = "";
-
-=======
 require_once "api/db.php";
->>>>>>> ff9d9d8dfc7e99533a15e6cde67f9a611bbc9300
 $error = "";
 
 $club_id = isset($_GET["id"]) ? (int)$_GET["id"] : (isset($_POST["club_id"]) ? (int)$_POST["club_id"] : 0);
